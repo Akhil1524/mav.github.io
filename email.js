b90a52055cf9466143c916ec308730bc71af54b8
Akhil1524 <157847672+Akhil1524@users.noreply.github.com>
2024-03-20 11:08:57 -0400

    Update email.js

--- email.js
+++ email.js
@@ -4,7 +4,7 @@ function showhideEmail(){
 		document.getElementById('email').innerHTMl = "Show my email";
 	shown=false;
 	}else{
-	var myemail = "<a href = 'mailto:chittina"+"@"+"mail.uc.edu'>chittina"+"@"+"ucmail.uc.edu</a>";
+	var myemail = "<a href = 'mailto:mav"+"@"+"mail.uc.edu'>mav"+"@"+"mail.uc.edu</a>";
 	document.getElementById('email').innerHTML=myemail;
 	shown = true;
 }
